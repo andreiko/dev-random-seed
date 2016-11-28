f56ab88f4771487a9a70d9a44cf481d34e4d7cbf
Andrey Bulgakov <mail@andreiko.ru>
2016-11-27 21:29:28 -0800

    Initial commit

---
+++ seed.go
@@ -0,0 +1,33 @@
+package dev_random_seed
+
+import (
+	"os"
+	"fmt"
+)
+
+const Filename = "/dev/random"
+
+func GetSeed() (int64, error) {
+	f, err := os.OpenFile(Filename, os.O_RDONLY, 0)
+	if err != nil {
+		return 0, err
+	}
+
+	var bytes [8]byte
+	n, err := f.Read(bytes[:])
+
+	if err != nil {
+		return 0, err
+	}
+
+	if n < len(bytes) {
+		return 0, fmt.Errorf("%d byte(s) instead of %d were read from %s", n, len(bytes), Filename)
+	}
+
+	var result int64
+	for i, v := range bytes {
+		result += int64(v) << uint(8 * i)
+	}
+
+	return result, nil
+}
